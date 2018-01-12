f4cc0e2306a6a885b231e536a0c91a9017ed1784
Antonin Jousson <joussonantonin@gmail.com>
2018-01-12 11:10:04 +0100

    first commit

---
+++ content/test.md
@@ -0,0 +1,5 @@
+Title: My First Article
+Date: 2018-01-08 12:51
+Category: Test
+
+This is a test article
---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Antonin Jousson'
+SITENAME = 'Neolin'
+SITEURL = 'neolin.me'
+
+# PATHS
+STATIC_PATHS = ['images', 'pdfs']
+
+THEME = "responsive"
+
+MINI_BIO = "Antonin mini bio"
+BIO = "Antonin's bio"
+
+
+PLUGIN_PATHS = ["/Users/Antonin/Documents/Coding stuff/Python/pelican-plugins/"]
+PLUGINS = ['neighbors']
+
+PATH = 'content'
+OUTPUT_PATH = 'output/'
+
+# Misc
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ( ('Github', 'https://github.com/Antoninj', '&#xe037;'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
